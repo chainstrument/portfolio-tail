01b191b9b719b73f421bca996c3060a012c21067
Chainstrument <minitutoriel@gmail.com>
2024-09-27 21:19:27 +0200

    init

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Sidenav from "./components/Sidenav";
+
+function App() {
+  return (
+    <>
+      <div className="container flex justify-center items-center">
+     
+        <Sidenav />
+      
+        <div className="w-52 h-52 bg-black flex rounded-full"> </div> 
+      
+
+      </div>
+
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Sidenav.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import { AiOutlineMenu } from 'react-icons/ai'
+
+const Sidenav = () => {
+  return (
+    <AiOutlineMenu />
+  )
+}
+
+export default Sidenav
